bce3c5542c984e6e69d48261c36f49a199e1889e
dustan1 <goadustan@gmail.com>
2014-09-05 15:05:57 -0400

    stuffs

--- dll.h
+++ dll.h
@@ -20,3 +20,27 @@ class DLLIMPORT DllClass
 
 
 #endif /* _DLL_H_ */
+
+
+
+
+
+
+
+
+
+
+//http://www.codeproject.com/Articles/655071/Basic-Cplusplus-Win-DLL
+//http://www.codeproject.com/Articles/28969/HowTo-Export-C-classes-from-a-DLL
+//http://msdn.microsoft.com/en-ca/library/1ez7dh12.aspx
+//http://www.ni.com/white-paper/3056/en/
+//http://msdn.microsoft.com/en-ca/library/ms235636.aspx
+//http://stackoverflow.com/questions/767579/exporting-classes-containing-std-objects-vector-map-etc-from-a-dll
+
+
+
+
+
+
+
+
