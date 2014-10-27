35c0085df6237cfae58281cb2b8c9763db2409fb
dustan1 <goadustan@gmail.com>
2014-10-26 22:42:07 -0400

    more things!

--- Libraries/Windows console stuff/ConsoleOptions.h
+++ Libraries/Windows console stuff/ConsoleOptions.h
@@ -2,7 +2,6 @@
 #define CONSOLEOPTIONS_H_INCLUDED
 
 #include "CursorOptions.h"
-#include "cstdlib"
 #include <cstdlib>
 #include <stdexcept>
 
@@ -13,10 +12,59 @@ public:
 
 COptions cursorOptions;
 
-int test();
 int setConsoleSize(unsigned int width, unsigned int height);
+void setConsoleTitle(std::string newName);
 
 };
+
+//Stuff is below
+
+
+int ConsoleOptions::setConsoleSize(unsigned int width, unsigned int height)
+{
+
+   SMALL_RECT r;
+   COORD      c;
+   CONSOLE_SCREEN_BUFFER_INFO csbi;
+   HANDLE hConOut = GetStdHandle( STD_OUTPUT_HANDLE );
+   if (!GetConsoleScreenBufferInfo( hConOut, &csbi )){
+      throw runtime_error( "You must be attached to a human." );
+   }
+
+      COORD maxSize1 = GetLargestConsoleWindowSize(hConOut);
+//   cout<<maxSize1.X << " " <<maxSize1.Y<<endl;
+
+   r.Left   = 0;
+   r.Top    = 0;
+   r.Right  = width -1;
+   r.Bottom = height -1;
+   SetConsoleWindowInfo( hConOut, TRUE, &r );
+
+   c.X = width;
+   c.Y = height;
+   SetConsoleScreenBufferSize( hConOut, c );
+
+   COORD maxSize = GetLargestConsoleWindowSize(hConOut);
+//   cout<<maxSize.X << " " <<maxSize.Y<<endl;
+
+}
+
+
+void ConsoleOptions::setConsoleTitle(std::string newName)
+{
+
+SetConsoleTitle(newName.c_str());
+
+
+//http://msdn.microsoft.com/en-us/library/windows/desktop/ms682073(v=vs.85).aspx
+//http://www.cplusplus.com/forum/windows/10731/
+}
+
+
+
+//Graphics!
+//http://lazyfoo.net/SDL_tutorials/lesson01/index.php
+//http://lazyfoo.net/tutorials/OpenGL/index.php
 
 
 
--- Libraries/Windows console stuff/CursorOptions.h
+++ Libraries/Windows console stuff/CursorOptions.h
@@ -42,4 +42,258 @@ void changeCurrentWindowText(const char* newText);//Changes the currently select
 
 };
 
+
+//Stuff is below
+
+
+
+
+//#define ENABLE_INSERT_MODE 0x0020
+//#define ENABLE_EXTENDED_FLAGS 0x0080
+
+using namespace std;
+
+COptions::COptions()
+{
+
+cursorVisibility = true;
+cursorSmall = true;
+currentColour=7;
+consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+}
+
+COptions::~COptions()
+{
+
+}
+
+void COptions::toggleCursor(HANDLE cHandle)
+{
+
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+
+   if (cursorVisibility){
+   cursorVisibility = false;
+   info.bVisible = cursorVisibility;
+   }
+   else
+   {
+   cursorVisibility = true;
+   info.bVisible = cursorVisibility;
+   }
+
+   SetConsoleCursorInfo(cHandle, &info);
+
+}
+
+void COptions::toggleCursorSize(HANDLE cHandle)
+{
+//doesn't do stuff
+   CONSOLE_CURSOR_INFO info;
+
+
+   if (cursorSmall){
+   cursorSmall=false;
+   info.dwSize = 100;
+   }
+   else
+   {
+   cursorSmall=true;
+   info.dwSize = 20;
+   }
+
+   SetConsoleCursorInfo(cHandle, &info);
+}
+
+int COptions::toggleColour(HANDLE cHandle)
+{
+
+    CONSOLE_SCREEN_BUFFER_INFO con_info;
+    GetConsoleScreenBufferInfo(cHandle, &con_info);
+    int cursorColour = con_info.wAttributes;
+
+    if (cursorColour>14){
+    cursorColour=0;
+    currentColour=0;
+    }
+
+    cursorColour+=1;
+    currentColour+=1;
+    SetConsoleTextAttribute(cHandle, cursorColour);
+
+    return cursorColour;
+    //0 is black, 1 is dark blue, 2 is green, 3 is cyan, 4 is dark red, 5 is magenta, 6 is gold, 7 is white, 8 is grey, 9 is blue, 10 is light green
+    //11 is light blue, 12 is red, 13 is magenta, 14 is yellow, 15 is light grey, 16+ is highlighted
+
+}// changes cursor colour
+
+void COptions::returnDefault(HANDLE cHandle)
+{
+
+    CONSOLE_SCREEN_BUFFER_INFO con_info;
+    GetConsoleScreenBufferInfo(cHandle, &con_info);
+    int cursorColour = con_info.wAttributes;
+
+    if (cursorColour!=15){
+    currentColour=15;
+    cursorColour=15;
+    SetConsoleTextAttribute(cHandle, cursorColour);
+    }
+
+    if (cursorVisibility){
+    toggleCursor(cHandle);
+    }
+
+    if (cursorSmall==false){
+    toggleCursorSize(cHandle);
+    }
+
+}// changes cursor colour
+
+void COptions::tempColourChange(bool isDiff, HANDLE cHandle, int targetColour)
+{
+   //if isDiff is true, change the colour to the target colour, otherwise, change it back
+
+    CONSOLE_SCREEN_BUFFER_INFO con_info;
+    GetConsoleScreenBufferInfo(cHandle, &con_info);
+    int cursorColour = con_info.wAttributes;
+
+    if(isDiff==false){
+    currentColour=cursorColour;
+    SetConsoleTextAttribute(cHandle, targetColour);
+    }
+
+    if(isDiff){
+    cursorColour=currentColour;
+    SetConsoleTextAttribute(cHandle, cursorColour);
+    }
+
+}
+
+void COptions::cursorControl(int optionNum)
+{
+     bool debugDetector=true;
+     if (optionNum == 0){
+
+        if (cursorVisibility==false){
+        toggleCursor(consoleHandle);
+        }
+
+        debugDetector=false;
+     }//if cursor is false, make true
+
+     if (optionNum == 1){
+
+        if (cursorVisibility){
+        toggleCursor(consoleHandle);
+        }
+
+        debugDetector=false;
+     }//if cursor is true, make false
+
+     if (optionNum==2){
+        toggleColour(consoleHandle);
+        debugDetector=false;
+     }//change colour
+
+     if (optionNum==3){
+        returnDefault(consoleHandle);
+        debugDetector=false;
+     }//returns defaults
+
+     if (optionNum==4){
+        toggleCursor(consoleHandle); //force toggle
+        debugDetector=false;
+     }
+
+     if (optionNum==5){
+        changeCursorInsert(consoleHandle, true);//Insert mode
+        debugDetector=false;
+     }
+
+     if (optionNum==6){
+        changeCursorInsert(consoleHandle, false);//no Insert mode
+        debugDetector=false;
+     }
+
+     if (optionNum==7){
+        tempColourChange(false, consoleHandle, 15);
+        debugDetector=false;
+     }
+
+     if (optionNum==8){
+        tempColourChange(true,consoleHandle, 15);
+        debugDetector=false;
+     }
+
+     if (optionNum==9){
+        //changeWindowText("Stuff", consoleHandle);
+        debugDetector=false;
+     }
+
+     //beyond this are unused
+
+     if (optionNum==10){
+        debugDetector=false;
+     }
+
+
+
+     if (debugDetector){
+     cout<<"Something broke in CursorOptions.cpp"<<endl;
+     }
+
+
+}
+
+
+
+
+bool COptions::changeCursorPos(short cursorX, short cursorY)
+{
+
+   int returningNum;
+   COORD coord;
+   coord.X = cursorX;
+   coord.Y = cursorY;
+
+   returningNum = SetConsoleCursorPosition(consoleHandle, coord);
+
+   if (returningNum == 0){
+      return false;
+   }
+
+   return true;
+}
+
+void COptions::changeCursorInsert(HANDLE cHandle, bool insertMode)
+{
+//DWORD mode = 0;
+//DWORD otherThing = ENABLE_INSERT_MODE | ENABLE_EXTENDED_FLAGS;
+//DWORD thing = ENABLE_EXTENDED_FLAGS;
+//GetConsoleMode(cHandle, &mode);
+
+   if (insertMode){
+   SetConsoleMode(cHandle, ENABLE_INSERT_MODE | ENABLE_EXTENDED_FLAGS);
+   }
+   else
+   {
+   SetConsoleMode(cHandle, ENABLE_EXTENDED_FLAGS);
+   }
+
+}
+
+void COptions::changeCurrentWindowText(const char* newText)//, HWND cHandle)
+{
+   HWND temp = GetActiveWindow();
+   HWND temp2 = GetForegroundWindow();
+   cout<<temp<<" "<<temp2<<endl;
+   cout<<SetWindowText(temp2, newText)<<endl;;
+}
+
+
+
+
 #endif
