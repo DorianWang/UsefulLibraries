1b6684dc864329059c5f79c55063ba5e1c912f22
dustan1 <goadustan@gmail.com>
2014-10-27 15:12:00 -0400

    stuff

---
+++ libraryController.h
@@ -0,0 +1,84 @@
+
+//Want a namespace?
+//http://stackoverflow.com/questions/4093407/c-namespaces-and-defining-classes-in-separate-files
+
+#ifdef INCLUDE_FILE_IO
+
+#ifndef FILEIO_H
+
+#include "\Libraries\FileIO\FileIO.h"
+
+#endif
+
+#endif // INCLUDE_FILE_IO
+
+
+#ifdef INCLUDE_BINARY_TREE
+
+#ifndef BINARY_TREE_H
+
+#include "\Libraries\Binary tree\binaryTree.h"
+
+#endif
+
+#endif // INCLUDE_BINARY_TREE
+
+
+
+#ifdef INCLUDE_ITEM_TREE
+
+#ifndef TREE_TYPE_H
+
+#include "\Libraries\itemTree\treeType.h"
+
+#endif
+
+#endif // INCLUDE_ITEM_TREE
+
+
+
+#ifdef INCLUDE_KEY_IO
+
+#ifndef KEYS_H
+
+#include "\Libraries\KeyIO\keys.h"
+
+#endif
+
+#endif // INCLUDE_KEY_IO
+
+
+#ifdef INCLUDE_LINKED_LIST
+
+#ifndef CONTAINER_H
+
+#include "\Libraries\Linked list\container.h"
+
+#endif
+
+#endif // INCLUDE_LINKED_LIST
+
+
+
+#ifdef INCLUDE_CONSOLE_OPTIONS
+
+#ifndef CONSOLEOPTIONS_H_INCLUDED
+
+#include "\Libraries\Windows console stuff\ConsoleOptions.h"
+
+#endif
+
+#endif // INCLUDE_CONSOLE_OPTIONS
+
+
+
+
+
+
+
+
+
+
+
+
+
